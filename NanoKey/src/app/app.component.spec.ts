67b25bfefaa04f60dff90a1ed520f72ac2600688
unknown <AGSPL16@AGSPL16.agspl.com>
2018-10-25 13:18:20 +0530

    pages added

--- NanoKey/src/app/app.component.spec.ts
+++ NanoKey/src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'NanoKey'`, async(() => {
+  it(`should have as title 'nanokey'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('NanoKey');
+    expect(app.title).toEqual('nanokey');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to NanoKey!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to nanokey!');
   }));
 });
--- NanoKey/src/app/app.module.ts
+++ NanoKey/src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: 'About', component: AboutComponent },
+  { path: 'Services', component: ServicesComponent },
+  { path: 'Contact', component: ContactComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ServicesComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
